410c6448bc425db4aacf0d6ef29c760d6be91fd0
keshavsoft <nknnkumar@yahoo.com>
2026-08-30 09:38:47 +0530

    form 3

--- test/v4/formTest.js
+++ test/v4/formTest.js
@@ -1,5 +1,5 @@
 import buildSpecElement from "../../webComponents/v7/domCreation/v2/buildSpecElement.js";
-import formGodSpec from "../../webComponents/v7/core/controls/form/v2/formGodSpec.json" with { type: "json" };
+import formGodSpec from "../../webComponents/v7/core/controls/form/v3/formGodSpec.json" with { type: "json" };
 
 const formContainer = document.getElementById("formContainer");
 
